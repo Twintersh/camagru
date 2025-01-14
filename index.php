067f1704ff61195948afbed8896a08dea56a8867
Twintersh <tom.wintersheim@gmail.com>
2025-01-14 17:30:47 +0100

    index page front

--- index.php
+++ index.php
@@ -1,16 +1,36 @@
-<html>
-    <head>
-        <title>My first PHP Website</title>
-    </head>
-    <body>
-        <h2>Login Page</h2>
-        <a href="index.php">Click here to go back<br/><br/>
-        <form action="checklogin.php" method="POST">
-           Enter Username: <input type="text"
-           name="username" required="required" /> <br/>
-           Enter password: <input type="password"
-           name="password" required="required" /> <br/>
-           <input type="submit" value="Login"/>
-        </form>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Camargu - Login</title>
+    <link rel="stylesheet" href="index.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div class="login-container">
+		<!-- why is it better to use FORM ? -->
+        <form class="login-form" method="POST">
+			<h1 class="txt">Camagru</h1>
+            <div class="input-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="Enter your username" required>
+            </div>
+            <div class="input-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter your password" required>
+            </div>
+            <button type="submit" class="login-button">Login</button>
+			<div class="form-footer">
+				<a href="#" class="link">Forgot your password?</a>
+				<p>Don't have an account? <a href="register.php" class="link">Sign up</a></p>
+			</div>
+		</form>
+	</div>
+	<footer class="footer">
+		<p>Made by twinters</p>
+	</footer>
+</body>
 </html>
